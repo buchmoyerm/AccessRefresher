8658058b22115a6cc09160464fe104ad008a23a5
Mark Buchmoyer <buchmoyerm@gmail.com>
2013-11-06 22:49:32 -0500

    Start working on how to find access object

---
+++ AccessRefresher/RefreshObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace AccessRefresher
+{
+    class RefreshObject
+    {
+        private Timer _refreshtimer;
+        private int _refreshInterval;
+        private Access _fromDb;
+        private Excel _toExcel;
+
+        private void OnRefresh()
+        {
+            
+        }
+    }
+}
---
+++ AccessRefresher/Util/WinApi.cs
@@ -0,0 +1,15 @@
+using System.Runtime.InteropServices;
+
+namespace AccessRefresher.Util
+{
+    static class WinApi
+    {
+        // AccessibleObjectFromWindow gets the IDispatch pointer of an object
+        // that supports IAccessible, which allows us to get to the native OM.
+        [DllImport("Oleacc.dll")]
+        private static extern int AccessibleObjectFromWindow(
+            int hwnd, uint dwObjectID,
+            byte[] riid,
+            ref Access.DocumentWindow ptr);
+    }
+}
